19d784597b2921c7d0728e5be8660136fe94a539
Jonathan Sanchez <jonathansanchez595@gmail.com>
2018-06-09 20:00:39 -0300

    3 links con diferentes caracteristicas

--- Topo_Mininet.py
+++ Topo_Mininet.py
@@ -80,7 +80,8 @@ class NetworkTopo( Topo ):
 def run():
     "Test linux router"
     topo = NetworkTopo()
-    net = Mininet( topo=topo , link=TCLink)  # controller is used by s1-s3
+    # net = Mininet( topo=topo , link=TCLink)  # controller is used by s1-s3
+    net = Mininet( topo=topo )  # controller is used by s1-s3
     net.start()
     info( '*** Routing Table on Router:\n' )
     info( net[ 'r0' ].cmd( 'route' ) )
