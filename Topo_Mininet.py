bd83dd5048f5f4073dc22429df262c478375b7d4
Jonathan Sanchez <jonathansanchez595@gmail.com>
2018-06-09 20:09:39 -0300

    3 links con diferentes caracteristicas

--- Topo_Mininet.py
+++ Topo_Mininet.py
@@ -59,12 +59,12 @@ class NetworkTopo( Topo ):
         linkopts_2 = dict(bw=30, delay='15ms', loss=5, use_htb=True)
         linkopts_3 = dict(bw=50, delay='20ms', loss=0, use_htb=True)
 
-        self.addLink( s1, router, intfName2='r0-eth1', cls=TCLink,
-                      params2={ 'ip' : defaultIP})  # for clarity
-        self.addLink( s2, router, intfName2='r0-eth2', cls=TCLink,
-                      params2={ 'ip' : defaultIP_2})
-        self.addLink( s3, router, intfName2='r0-eth3', cls=TCLink,
-                      params2={ 'ip' : defaultIP_3})
+        self.addLink( s1, router, intfName2='r0-eth1',
+                      params2={ 'ip' : defaultIP}, cls=TCLink)  # for clarity
+        self.addLink( s2, router, intfName2='r0-eth2',
+                      params2={ 'ip' : defaultIP_2}, cls=TCLink)
+        self.addLink( s3, router, intfName2='r0-eth3',
+                      params2={ 'ip' : defaultIP_3}, cls=TCLink)
 
         h1 = self.addHost( 'h1', ip='192.168.1.100/24',
                            defaultRoute='via 192.168.1.1' )
